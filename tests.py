2475542b2bec6ce616eeb66002fff4bd3d8f9694
glenn mohre <glenn.mohre@gmail.com>
2017-02-27 22:33:03 -0500

    fixed imports of cards

--- tests.py
+++ tests.py
@@ -1,35 +1,36 @@
-from cards import cards
+from cards.cards import *
 from game_state import GameState
 from agent import Agent
 from netty_1 import Netty
 from timeit import timeit
 import numpy as np
 from util import one_hot
+
 class BelchAIrtestClass():
     deck_dict = {
-        cards.Taiga(): (1, 0),
-        cards.ElvishSpiritGuide(): (4, 0),
-        cards.SimianSpiritGuide(): (4, 0),
-        cards.TinderWall(): (4, 0),
-        cards.DesperateRitual(): (4, 0),
-        cards.PyreticRitual(): (3, 0),
-        cards.RiteOfFlame(): (4, 0),
-        cards.SeethingSong(): (4, 0),
-        cards.EmptyTheWarrens(): (3, 1),
-        cards.GitaxianProbe(): (4, 0),
-        cards.LandGrant(): (4, 0),
-        cards.LionsEyeDiamond(): (4, 0),
-        cards.LotusPetal(): (4, 0),
-        cards.Manamorphose(): (2, 0),
-        cards.GoblinCharbelcher(): (4, 0),
-        cards.ChromeMox(): (3, 0),
-        cards.BurningWish(): (4, 0),
-        cards.ReforgeTheSoul(): (0, 1)
+        Taiga(): (1, 0),
+        ElvishSpiritGuide(): (4, 0),
+        SimianSpiritGuide(): (4, 0),
+        TinderWall(): (4, 0),
+        DesperateRitual(): (4, 0),
+        PyreticRitual(): (3, 0),
+        RiteOfFlame(): (4, 0),
+        SeethingSong(): (4, 0),
+        EmptyTheWarrens(): (3, 1),
+        GitaxianProbe(): (4, 0),
+        LandGrant(): (4, 0),
+        LionsEyeDiamond(): (4, 0),
+        LotusPetal(): (4, 0),
+        Manamorphose(): (2, 0),
+        GoblinCharbelcher(): (4, 0),
+        ChromeMox(): (3, 0),
+        BurningWish(): (4, 0),
+        ReforgeTheSoul(): (0, 1)
     }
 
     def setup_test_gamestate(self):
         self.test_game_state = GameState()
-        self.test_game_state.add_deck(deck_dict)
+        self.test_game_state.add_deck(self.deck_dict)
 
     def setup_game(self):
         self.game_state = GameState()
@@ -44,12 +45,12 @@ class BelchAIrtestClass():
         del self.test_game_state
 
     def testGamestate(self):
-        # test_game_state.reset_game(False)
+        self.test_game_state.reset_game(False)
         print('Successfully reset game')
         assert True
 
     def testGame(self):
-        # game_state.reset_game()
-        # game_state.all_actions()
+        self.game_state.reset_game()
+        self.game_state.all_actions()
         print('Sucessfully ran all actions')
         assert True
